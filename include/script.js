46d6a64ff26c2b4bc184da8093db9ed8c1dce647
CodeDigiLab <159695671+CodeDigiLab@users.noreply.github.com>
2024-10-26 17:26:43 +0530

    ergyrt

---
+++ include/script.js
@@ -0,0 +1,163 @@
+document.write(`
+
+
+
+
+
+<div class="mobile-nav__wrapper">
+<div class="mobile-nav__overlay mobile-nav__toggler"></div>
+<!-- /.mobile-nav__overlay -->
+<div class="mobile-nav__content">
+    <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
+
+    <div class="logo-box">
+        <a href="index.html" aria-label="logo image"><img src="assets/images/logo-light.png" width="155"
+                alt="" /></a>
+    </div>
+    <!-- /.logo-box -->
+    <div class="mobile-nav__container"></div>
+    <!-- /.mobile-nav__container -->
+
+    <ul class="mobile-nav__contact list-unstyled">
+        <li>
+            <i class="fa fa-envelope"></i>
+            <a href="mailto:needhelp@tolak.com">needhelp@tolak.com</a>
+        </li>
+        <li>
+            <i class="fa fa-phone-alt"></i>
+            <a href="tel:666-888-0000">666 888 0000</a>
+        </li>
+    </ul><!-- /.mobile-nav__contact -->
+    <div class="mobile-nav__social">
+        <a href="https://facebook.com/">
+            <i class="fab fa-facebook-f" aria-hidden="true"></i>
+            <span class="sr-only">Facebook</span>
+        </a>
+        <a href="https://pinterest.com/">
+            <i class="fab fa-pinterest-p" aria-hidden="true"></i>
+            <span class="sr-only">Pinterest</span>
+        </a>
+        <a href="https://twitter.com/">
+            <i class="fab fa-twitter" aria-hidden="true"></i>
+            <span class="sr-only">Twitter</span>
+        </a>
+        <a href="https://instagram.com/">
+            <i class="fab fa-instagram" aria-hidden="true"></i>
+            <span class="sr-only">Instagram</span>
+        </a>
+    </div><!-- /.mobile-nav__social -->
+</div>
+<!-- /.mobile-nav__content -->
+</div>
+<!-- /.mobile-nav__wrapper -->
+<div class="search-popup">
+<div class="search-popup__overlay search-toggler"></div>
+<!-- /.search-popup__overlay -->
+<div class="search-popup__content">
+    <form role="search" method="get" class="search-popup__form" action="#">
+        <input type="text" id="search" placeholder="Search Here..." />
+        <button type="submit" aria-label="search submit" class="tolak-btn">
+            <b><i class="icon-magnifying-glass"></i></b><span></span>
+        </button>
+    </form>
+</div>
+<!-- /.search-popup__content -->
+</div>
+<!-- /.search-popup -->
+<!-- Sidebar One Start -->
+<aside class="sidebar-one">
+<div class="sidebar-one__overlay"></div><!-- /.siderbar-ovarlay -->
+<div class="sidebar-one__content">
+    <div class="sidebar-one__close"><i class="icon-close"></i></div><!-- /.siderbar-close -->
+    <div class="sidebar-one__logo">
+        <a href="index.html" aria-label="logo image"><img src="assets/images/logo-light.png" alt="Tolak HTML"
+                width="184"></a>
+    </div><!-- /.sidebar-one__logo-box -->
+    <p class="sidebar-one__text">
+        Mauris ut enim sit amet lacus ornare ullamcor. Praesent placerat nequ
+        puru rhoncu tincidunt odio ultrices. Sed feugiat feugiat felis.
+    </p>
+    <h4 class="sidebar-one__title">Contact Info:</h4>
+    <ul class="sidebar-one__info">
+        <li>
+            <span class="fas fa-map-marker-alt"></span>
+            27, Dhaka London City Dhaka, Bangladesh
+        </li>
+        <li>
+            <span class="fas fa-clock"></span>
+            Mon - Fri: 8.00 am. - 6.00 pm.
+        </li>
+        <li>
+            <span class="fas fa-envelope"></span>
+            <a href="tel:09969569535">099 695 695 35</a>
+        </li>
+    </ul>
+    <div class="sidebar-one__social">
+        <a href="https://facebook.com/">
+            <i class="fab fa-facebook-f" aria-hidden="true"></i>
+            <span class="sr-only">Facebook</span>
+        </a>
+        <a href="https://pinterest.com/">
+            <i class="fab fa-pinterest-p" aria-hidden="true"></i>
+            <span class="sr-only">Pinterest</span>
+        </a>
+        <a href="https://twitter.com/">
+            <i class="fab fa-twitter" aria-hidden="true"></i>
+            <span class="sr-only">Twitter</span>
+        </a>
+        <a href="https://instagram.com/">
+            <i class="fab fa-instagram" aria-hidden="true"></i>
+            <span class="sr-only">Instagram</span>
+        </a>
+    </div><!-- /sidebar-one__socila -->
+    <h4 class="sidebar-one__title">Newsletter:</h4>
+    <form action="#" data-url="MAILCHIMP_FORM_URL" class="sidebar-one__newsletter mc-form">
+        <input type="text" name="EMAIL" placeholder="Email address">
+        <button type="submit" class="fas fa-paper-plane">
+            <span class="sr-only">submit</span><!-- /.sr-only -->
+        </button>
+    </form><!-- /.footer-widget__newsletter mc-form -->
+</div><!-- /.sidebar__content -->
+</aside>
+<!-- Sidebar One Start -->
+
+<a href="#" data-target="html" class="scroll-to-target scroll-to-top">
+<span class="scroll-to-top__text">back top</span>
+<span class="scroll-to-top__wrapper"><span class="scroll-to-top__inner"></span></span>
+</a>
+
+
+<script src="assets/vendors/jquery/jquery-3.7.0.min.js"></script>
+<script src="assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
+<script src="assets/vendors/jarallax/jarallax.min.js"></script>
+<script src="assets/vendors/jquery-ui/jquery-ui.js"></script>
+<script src="assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js"></script>
+<script src="assets/vendors/jquery-appear/jquery.appear.min.js"></script>
+<script src="assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js"></script>
+<script src="assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="assets/vendors/jquery-validate/jquery.validate.min.js"></script>
+<script src="assets/vendors/nouislider/nouislider.min.js"></script>
+<script src="assets/vendors/tiny-slider/tiny-slider.js"></script>
+<script src="assets/vendors/wnumb/wNumb.min.js"></script>
+<script src="assets/vendors/owl-carousel/js/owl.carousel.min.js"></script>
+<script src="assets/vendors/wow/wow.js"></script>
+<script src="assets/vendors/tilt/tilt.jquery.js"></script>
+<script src="assets/vendors/simpleParallax/simpleParallax.min.js"></script>
+<script src="assets/vendors/imagesloaded/imagesloaded.min.js"></script>
+<script src="assets/vendors/isotope/isotope.js"></script>
+<script src="assets/vendors/countdown/countdown.min.js"></script>
+<script src="assets/vendors/jquery-circleType/jquery.circleType.js"></script>
+<script src="assets/vendors/jquery-lettering/jquery.lettering.min.js"></script>
+<script src="assets/vendors/progress-bar/knob.js"></script>
+
+<!-- chart js -->
+<script src="assets/vendors/chart/chart.js"></script>
+<script src="assets/vendors/chart/custome-chart.js"></script>
+
+<!-- template js -->
+<script src="assets/js/tolak.js"></script>
+
+
+
+`);
