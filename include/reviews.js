872b4993bba027fc74df17ccb7e8a667931efe9b
CodeDigiLab <159695671+CodeDigiLab@users.noreply.github.com>
2024-10-28 18:19:01 +0530

    dfgdfg

---
+++ include/reviews.js
@@ -0,0 +1,17 @@
+document.write(`
+
+
+<section class="service-details" style="padding: 0px;">
+<div class="container-fluid" style="padding:1px; overflow: hidden;">
+    <div class="row">
+        <div class="col-sm-12">
+            <img style="object-fit:contain;" class="d-block w-100"
+                src="assets/images/writethesistoday/review.jpg" alt="writethesistoday">
+
+        </div>
+    </div>
+</div>
+</section>
+
+
+`);
---
+++ include/sectiongif.js
@@ -0,0 +1,18 @@
+document.write(`
+
+
+<section class="service-details mt-5" style="padding: 0px;">
+            <div class="container-fluid" style="padding:1px; overflow: hidden;">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <img style="object-fit:contain;" class="d-block w-100"
+                            src="assets/images/writethesistoday/assignments.gif" alt="writethesistoday">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+`);
